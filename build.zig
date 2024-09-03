54d838ac799c71a407f97b043cfe307406680905
thoxy <thi.hur67@gmail.com>
2024-09-03 18:24:47 +0200

    make build.zig also build examples

--- build.zig
+++ build.zig
@@ -3,8 +3,7 @@ const std = @import("std");
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
-pub fn build(b: *std.Build) void
-{
+pub fn build(b: *std.Build) void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -16,15 +15,13 @@ pub fn build(b: *std.Build) void
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    _ = b.addModule("zon_get_fields", // package_name
-    .{
+    const zon_get_fields = b.addModule("zon_get_fields", .{
         .root_source_file = b.path("src/zon_get_fields.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const lib = b.addStaticLibrary(
-    .{
+    const lib = b.addStaticLibrary(.{
         .name = "zon_get_fields",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
@@ -40,8 +37,7 @@ pub fn build(b: *std.Build) void
 
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
-    const lib_unit_tests = b.addTest(
-    .{
+    const lib_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/zon_get_fields.zig"),
         .target = target,
         .optimize = optimize,
@@ -53,4 +49,16 @@ pub fn build(b: *std.Build) void
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
+
+    // Add examples build
+    inline for ([_][]const u8{ "example_get_field_val", "example_zon_to_struct" }) |example| {
+        const exe = b.addExecutable(.{
+            .name = example,
+            .root_source_file = b.path("examples/" ++ example ++ ".zig"),
+            .target = target,
+            .optimize = optimize,
+        });
+        exe.root_module.addImport("zon_get_fields", zon_get_fields);
+        b.installArtifact(exe);
+    }
 }
